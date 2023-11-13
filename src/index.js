39188b1ba2ec2741a221d7d5983afa7bf284ed98
franco1988 <francoplazafc@gmail.com>
2023-11-13 10:32:20 -0300

    index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './containers/App';
 
 ReactDOM.render(
-  <BrowserRouter basename="/AppClima">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
