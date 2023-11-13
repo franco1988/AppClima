11d71e0fc934d553f387919ab9ad4f243971ff39
franco1988 <francoplazafc@gmail.com>
2023-11-13 10:28:45 -0300

    ghpages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './containers/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/AppClima">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
